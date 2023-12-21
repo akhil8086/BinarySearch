03436ef9af324a718b4539795f39065542ec4a0b
akhil8086 <akhiljith.r@edstem.com>
2023-12-21 12:57:28 +0530

    committed

--- assessment/src/Components/BInarySearch.jsx
+++ assessment/src/Components/BInarySearch.jsx
@@ -79,45 +79,4 @@ export default BinarySearch;
 
 
 
-// import { useState } from 'react'
 
-
-// const BinarySearch = () => {
-//   const [ insertValue, setInsertValue ] = useState(0)
-//   var exarr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-//   const binarySearch = (arr, val) => {
-//     let start = 0, end = arr.length - 1
-
-//     while (start <= end) {
-//       let mid = Math.floor((start + end)/2)
-//       console.log(mid)
-//       if (arr[mid] === val) {
-//         return mid
-//       }
-
-//       if (val < arr[mid]) {
-//         end = mid - 1
-//       } else {
-//         start = mid + 1
-//       }
-//     }
-//     return -1
-//   }
-
-
-//   return (
-//     <div>
-//       <br />
-//       <hr />
-//      <div style={{border:"1px solid red"}}>
-//         <h1>Binary Search</h1>
-//         <h4>Array = {JSON.stringify(exarr)}</h4>
-//         <h4>Search <input type="number" onChange={e => setInsertValue(e.target.value)} /></h4>
-//         <h3>Search {insertValue}, Result in location: {binarySearch(exarr, +insertValue)}</h3>
-//       </div>
-//       <hr />
-//     </div>
-//   )
-// }
-
-// export default BinarySearch
